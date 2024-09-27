9e05be3d097b247fb95c52294d4f16dd32cd416e
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-09-27 12:19:28 +0600

    spacecount.c

---
+++ spacecount.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+ char str[100];
+ puts("Enter a string ");
+ gets(str);
+
+ int space_count=0;
+ for(int i=0;str[i]!='\0';i++){
+   if(str[i]==' ')
+    space_count++;
+}
+
+printf("Space_count = %d",space_count);
+
+return 0;
+
+}
