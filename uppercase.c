726c9bc413a89e14865cb715c80b1de1095c1fee
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-09-27 17:28:48 +0600

    uppercase.c

---
+++ uppercase.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  char str[1000];
+  puts("Enter a string :");
+  gets(str);
+
+  for(int i=0;str[i]!='\0';i++){
+    if(str[i]>='a' && str[i]<='z'){
+        str[i]=str[i]-32;
+    }
+  }
+  puts(str);
+
+}
