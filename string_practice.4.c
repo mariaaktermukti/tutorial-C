8f41da6e61fb5c9fbd12001b4b46cd9d8b8001aa
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-09-30 21:55:31 +0600

    Add files via upload

---
+++ string_practice.4.c
@@ -0,0 +1,19 @@
+/* upper thekle lower, lower thakle upper */
+#include<stdio.h>
+int main(){
+    char str[1000];
+ puts("Enter the string : ");
+ gets(str);
+  for(int i=0;str[i]!='\0';i++){
+    if(str[i]>=65 && str[i]<=90){
+        str[i]=str[i]+32;
+    }
+    else if(str[i]>=97 && str[i]<=122){
+        str[i]=str[i]-32;
+    }
+
+  }
+  puts(str);
+
+
+}
