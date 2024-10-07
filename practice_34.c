8639528a69a6276ad27f42cb1213f4b1f3c612d8
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-07 23:36:14 +0600

    practice_34.c

--- practice_34.c
+++ practice_34.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
 
 int main() {
-   long long int a,b,c,d;
-   scanf("%lld %lld %lld", &a, &b, &c);
-   if ((a + b + c == d) || (a + b - c == d) || (a - b - c == d) || (a - b + c == d) ||
-        (a * b + c == d) || (a + b * c == d) || (a * b - c == d) || (a - b * c == d) ||
-        (a * b * c == d)){
-    printf("YES\n");
-   }
-   else{
-    printf("NO\n");
-   }
+    long long a, b, c, d;
+    scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+    if ( a + b + c == d ||
+        a + b - c == d ||
+        a + b * c == d ||
+        a - b + c == d ||
+        a - b - c == d ||
+        a - b * c == d ||
+        a * b + c == d ||
+        a * b - c == d ||
+        a * b * c == d ||
+        b + a + c == d ||
+        b + a - c == d ||
+        b + a * c == d ||
+        b - a + c == d ||
+        b - a - c == d ||
+        b - a * c == d ||
+        b * a + c == d ||
+        b * a - c == d ||
+        b * a * c == d || 
+        c + a + b == d ||
+        c + a - b == d ||
+        c + a * b == d ||
+        c - a + b == d ||
+        c - a - b == d ||
+        c - a * b == d ||
+        c * a + b == d ||
+        c * a - b == d ||
+        c * a * b == d
+    ) {
+        printf("YES\n");
+    } 
+    else {
+        printf("NO\n");
+    }
+
     return 0;
 }
+
