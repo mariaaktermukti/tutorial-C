f368f256c034256c26c22f5bd6226f13e3195d9e
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-07 17:32:30 +0600

    practice_26.c

---
+++ practice_26.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int password;
+    while (1) {
+        scanf("%d", &password); 
+        
+        if (password == 1999) {
+            printf("Correct\n");
+            break; 
+        } 
+        else {
+            printf("Wrong\n"); 
+        }
+    }
+
+    return 0;
+}
