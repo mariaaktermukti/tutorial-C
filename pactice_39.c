5a9d42ba5a0eb60bbb1da150eb1b0edbb2dc2207
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-08 09:12:20 +0600

    practice_39.c

---
+++ pactice_39.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    int a = N / 10;   
+    int b = N % 10;  
+    if (a % b == 0 || b % a == 0) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+    
+    return 0;
+}
