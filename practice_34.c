13d210f4d22c21c6dbf266c49e72c3316ac31ba3
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-08 07:28:09 +0600

    practice_36.c

--- practice_34.c
+++ practice_34.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
 int main() {
-    long long a, b, c, d;
+    long long int a, b, c, d;
     scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
-    if ( a + b + c == d ||
+
+    if (
+        a + b + c == d ||
         a + b - c == d ||
         a + b * c == d ||
         a - b + c == d ||
@@ -20,7 +21,7 @@ int main() {
         b - a * c == d ||
         b * a + c == d ||
         b * a - c == d ||
-        b * a * c == d || 
+        b * a * c == d ||
         c + a + b == d ||
         c + a - b == d ||
         c + a * b == d ||
@@ -29,14 +30,22 @@ int main() {
         c - a * b == d ||
         c * a + b == d ||
         c * a - b == d ||
-        c * a * b == d
+        c * a * b == d ||
+        (a + b) * c == d ||
+        (a - b) * c == d ||
+        (a * b) + c == d ||
+        (a + c) * b == d ||
+        (a - c) * b == d ||
+        (a * c) + b == d ||
+        (b + c) * a == d ||
+        (b - c) * a == d ||
+        (b * c) + a == d
     ) {
         printf("YES\n");
-    } 
-    else {
+    }
+     else {
         printf("NO\n");
     }
 
     return 0;
 }
-
