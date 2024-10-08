d9424441a84c98ba90be2ef3151a5bc967d1c893
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-08 09:17:42 +0600

    practice_39.c

--- pactice_39.c
+++ pactice_39.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 int main() {
     int N;
-    scanf("%d", &N);  
+    scanf("%d", &N); 
     int a = N / 10;  
-    int b = N % 10;  
-    if (b != 0 && (a % b == 0 || b % a == 0)) {
+    int b = N % 10;   
+    if (b == 0) {
+        if (a != 0) {  
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    } else if (a % b == 0 || b % a == 0) {
         printf("YES\n");
     } else {
         printf("NO\n");
