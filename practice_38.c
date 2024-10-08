8275fba4e89729a20ee06e44cd74bc69e2757da8
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-08 09:05:36 +0600

    practice_38.c

---
+++ practice_38.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    int hasEven = 0; 
+    for (int i = 2; i <= N; i += 2) { 
+        printf("%d\n", i);
+        hasEven = 1; 
+    }
+    if (hasEven == 0) {
+        printf("-1\n");
+    }
+
+    return 0;
+}
