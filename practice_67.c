a2c2e5c28481e989656c1d6e0432e4c8c6b161b0
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-11 17:11:49 +0600

    practice_67.c

---
+++ practice_67.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <limits.h>
+int main() {
+    int N, K;
+    scanf("%d %d", &N, &K); 
+    int A[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &A[i]); 
+    }
+
+    for (int i = 0; i < N; i += K) {
+        int min_value = INT_MAX;  
+        for (int j = i; j < i + K && j < N; j++) {
+            if (A[j] < min_value) {
+                min_value = A[j];
+            }
+        }
+        printf("%d ", min_value);
+    }
+
+    printf("\n");
+    return 0;
+}
