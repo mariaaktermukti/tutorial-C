652d901b72fe98faf115e8b3b56e6ac76151ddfe
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-12 18:31:28 +0600

    practice_74.c

--- practice_75.c
+++ practice_75.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main() {
-    double R;
-    scanf("%lf", &R);
-    const double PI = 3.141592653;
+    double R; 
+    const double PI = 3.141592653; 
+    if (scanf("%lf", &R) != 1) {
+        printf("Invalid input\n");
+        return 1; 
+    }
     double area = PI * R * R;
+
     printf("%.9lf\n", area);
 
     return 0;
