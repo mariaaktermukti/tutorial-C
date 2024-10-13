c10419eb34ec26a48f967bf27d8f54d755d4765c
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-13 15:16:49 +0600

    practice_77.c

--- practice_77.c
+++ practice_77.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 int main() {
-    int x, y;
-    scanf("%d %d", &x, &y);
-    printf("%d + %d = %d\n", x, y, x + y);
-    printf("%d * %d = %d\n", x, y, x * y);
-    printf("%d - %d = %d\n", x, y, x - y);
+    int X,Y;
+    long long int multiplication;  
+    scanf("%d %d", &X, &Y);
+    multiplication = (long long int)X * Y;
+
+    printf("%d + %d = %d\n", X, Y, X + Y);
+    printf("%d * %d = %lld\n", X, Y, multiplication);
+    printf("%d - %d = %d\n",  X, Y, X - Y);
     
     return 0;
 }
