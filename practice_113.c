9a09268b54937173d6aca1f9f58fae2aeed54924
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-20 23:37:02 +0600

    practice_113.c

---
+++ practice_113.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    char str[200];
+    int n;
+    scanf("%d", &n);
+    scanf("%s", str);
+    
+    int num=0;
+    for(int i=0;i<n;i++){
+        int digit=str[i]-'0';
+
+        num= num*10+ digit;
+    }
+    printf("%d\n", num);
+    return 0;
+}
