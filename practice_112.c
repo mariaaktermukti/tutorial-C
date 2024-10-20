0bf5c65f19bede30799042294db41b969ed34cda
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-20 23:22:30 +0600

    practice_112.c

---
+++ practice_112.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char str[5]="5";
+    printf("%d\n", str[0]-48);
+    printf("%d", str[0]-'0');
+     
+    return 0;
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,13 +1,1 @@
-#include<stdio.h>
-int main(){
-  char str[1000];
-  puts("Enter a string :");
-  gets(str);
-  for(int i=0;str[i]!='\0';i++){
-    if(str[i]>=65 && str[i]<=90)
-     str[i]=str[i]+32;
-    else if (str[i]>=97 && str[i]<=122)
-     str[i]=str[i]-32;
-  }
-  puts(str);
-}
+ // printf("%d", str[0]-48);
