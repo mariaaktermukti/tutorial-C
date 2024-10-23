20c3899101e4651e2857ba3fe5326fde82246031
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-23 19:41:23 +0600

    file_append.c

---
+++ file_append.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    FILE *fp;
+    fp= fopen("exam.txt", "a");
+
+    if(fp == NULL){
+        printf("\n Not found");
+        return 1;
+    }
+    else{
+        printf("\n File open in append mode.");
+        fputs("write something in it", fp);
+    }
+    fclose(fp);
+    return 0;
+}
