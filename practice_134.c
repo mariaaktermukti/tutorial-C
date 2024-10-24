bb1277280c7e2a0c70fd126be33d959c2c5a35da
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-24 21:02:32 +0600

    practice_134.c

---
+++ practice_134.c
@@ -0,0 +1,12 @@
+/*Code force__ (string)*/
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char S[1004], T[1004];  
+    scanf("%s", S);
+    scanf("%s", T);
+    printf("%d %d\n", strlen(S), strlen(T));
+    printf("%s %s\n", S, T);
+
+    return 0;
+}
