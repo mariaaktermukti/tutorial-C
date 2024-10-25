e03671ba92955470d2fefc4171a8147ec3106274
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-25 11:46:07 +0600

    practice_137.c

--- practice_137.c
+++ practice_137.c
@@ -1,1 +1,13 @@
 /*Code force__ (string_E)*/
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char S[1000001];
+    scanf("%s", S);  
+    int sum = 0;
+    for (int i = 0; S[i] != '\0'; i++) {
+        sum += S[i] - '0'; 
+    }
+    printf("%d\n", sum); 
+    return 0;
+}
