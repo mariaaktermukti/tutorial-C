8a6b58435a6db774f3dbaf829446e660a4667db8
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-29 10:08:27 +0600

    practice_148.c

--- practice_148.c
+++ practice_148.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 int main() {
     int N, M, X;
-    int found = 0;  
-    scanf("%d %d", &N, &M);
-    scanf("%d", &X);
+    int found = 0;
+
+    scanf("%d %d", &N, &M);  
+
     int A[N][M];
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            scanf("%d", &A[i][j]);  
-            if (A[i][j] == X) {
-                found = 1; 
-            }
+            scanf("%d", &A[i][j]);
         }
     }
+    
+    scanf("%d", &X);
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (A[i][j] == X) {
+                found = 1;
+                break; 
+            }
+        }
+        if (found) break;
+    }
 
-   
     if (found) {
         printf("will not take number\n");
     } 
