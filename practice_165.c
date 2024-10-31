4f80aa5d9ffe7bc743f2bd171d3253a0a0914936
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-31 22:29:59 +0600

    practice_165.c

--- practice_165.c
+++ practice_165.c
@@ -1,30 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 1000001
-int hashSet[MAX];
+// Comparison function for qsort
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+// Function to check if X is present in sorted array A using binary search
+const char* binary_search(int *arr, int size, int x) {
+    int *item = (int *)bsearch(&x, arr, size, sizeof(int), compare);
+    return (item != NULL) ? "found" : "not found";
+}
+
 int main() {
     int N, Q;
+
+    // Read N (size of array) and Q (number of queries)
     scanf("%d %d", &N, &Q);
-    for (int i = 0; i < MAX; i++) {
-        hashSet[i] = 0;
+
+    // Allocate memory for array A
+    int *A = (int *)malloc(N * sizeof(int));
+    if (A == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
     }
+
+    // Read elements of array A
     for (int i = 0; i < N; i++) {
-        int num;
-        scanf("%d", &num);
-        hashSet[num] = 1;  
+        scanf("%d", &A[i]);
     }
-    
+
+    // Sort array A to enable binary search
+    qsort(A, N, sizeof(int), compare);
+
+    // Process each query
     for (int i = 0; i < Q; i++) {
         int X;
         scanf("%d", &X);
-        if (hashSet[X]) {
-            printf("found\n");
-        } 
-        else {
-            printf("not found\n");
-        }
+        printf("%s\n", binary_search(A, N, X));
     }
+
+    // Free allocated memory
+    free(A);
 
     return 0;
 }
