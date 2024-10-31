11b1564c81068ab69d0b0649da0b9c85205a7ce6
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-31 23:40:21 +0600

    practice_168.c

--- practice_168.c
+++ practice_168.c
@@ -1,13 +1,11 @@
 /*HackerRank*/
 #include <stdio.h>
-
 int main() {
     int N;
     scanf("%d", &N);
     int matrix[N][N];
-    
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
             scanf("%d", &matrix[i][j]);
         }
     }
