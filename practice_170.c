8b0b89f1f2bf3eee667418e6cf26ecba689c4a54
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-10-31 23:51:48 +0600

    practice_170.c

---
+++ practice_170.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define MAX_NUM 100000
+
+int main() {
+    int N;
+    scanf("%d", &N); 
+
+    int arr[N];
+    int count[MAX_NUM + 1] = {0};
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+        count[arr[i]]++;  
+    }
+
+    int unique_count = 0;
+    for (int i = 1; i <= MAX_NUM; i++) {
+        if (count[i] == 1) {
+            unique_count++; 
+        }
+    }
+
+    printf("%d\n", unique_count); 
+
+    return 0;
+}
