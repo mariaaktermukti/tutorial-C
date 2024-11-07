e7a91b963ac1a0fcb92725c2f5c1cf102e990e2b
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-07 08:49:45 +0600

    practice_192.c

---
+++ practice_192.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int findMin(int arr[], int n) {
+    int min = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+int findMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int min = findMin(arr, n);
+    int max = findMax(arr, n);
+
+    printf("%d %d\n", min, max);
+
+    return 0;
+}
