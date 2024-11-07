22e160e68a0ed5bef657f08a9ab95410ae95a07b
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-07 08:43:06 +0600

    practice_191.c

---
+++ practice_191.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// Function to calculate X raised to the power of exp
+int power(int X, int exp) {
+    int result = 1;
+    for (int i = 0; i < exp; i++) {
+        result *= X;
+    }
+    return result;
+}
+
+// Function to calculate the sum S
+int calculateSum(int X, int N) {
+    int sum = 0;
+    for (int i = 0; i <= N; i += 2) {
+        if (i == 0) {
+            sum += (power(X, i) - 1); // For X^0 - 1
+        } else {
+            sum += power(X, i);       // For X^2, X^4, ..., X^N
+        }
+    }
+    return sum;
+}
+
+int main() {
+    int X, N;
+    scanf("%d %d", &X, &N);
+
+    int result = calculateSum(X, N);
+    printf("%d\n", result);
+
+    return 0;
+}
