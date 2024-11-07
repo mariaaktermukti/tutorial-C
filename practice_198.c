3a6178d91b75f6151999e8d331b7416f2e23f7d3
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-07 10:32:06 +0600

    practice_198.c

--- practice_198.c
+++ practice_198.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
 void printBinary(int n) {
+    // Array to store binary digits
+    int binary[32];  // Since N <= 10^9, 32 bits are sufficient
+    int index = 0;
+
+    // Handle the case where n is zero directly
     if (n == 0) {
+        printf("0");
         return;
     }
-    printBinary(n / 2);
-    printf("%d", n % 2);
+
+    // Convert decimal to binary by dividing by 2
+    while (n > 0) {
+        binary[index++] = n % 2;
+        n = n / 2;
+    }
+
+    // Print the binary digits in reverse order
+    for (int i = index - 1; i >= 0; i--) {
+        printf("%d", binary[i]);
+    }
 }
 
 int main() {
@@ -15,15 +30,10 @@ int main() {
     while (T--) {
         int N;
         scanf("%d", &N);
-        
-        if (N == 0) {
-            printf("0");  
-        } else {
-            printBinary(N);
-        }
-        
-        printf("\n");  
+
+        printBinary(N);
+        printf("\n");  // Move to the next line for each test case
     }
-    
+
     return 0;
 }
