38ab86fbafad4f562e43a26ea5bb9468223a729f
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-07 08:46:00 +0600

    practice_191.c

--- practice_191.c
+++ practice_191.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Function to calculate X raised to the power of exp
+// Function to calculate X raised to the power of exp without using built-in functions
 int power(int X, int exp) {
     int result = 1;
     for (int i = 0; i < exp; i++) {
@@ -9,14 +9,14 @@ int power(int X, int exp) {
     return result;
 }
 
-// Function to calculate the sum S
+// Function to calculate the sum S as specified
 int calculateSum(int X, int N) {
     int sum = 0;
     for (int i = 0; i <= N; i += 2) {
         if (i == 0) {
-            sum += (power(X, i) - 1); // For X^0 - 1
+            sum += (power(X, i) - 1); // X^0 - 1
         } else {
-            sum += power(X, i);       // For X^2, X^4, ..., X^N
+            sum += power(X, i);       // X^2, X^4, ..., X^N
         }
     }
     return sum;
@@ -25,7 +25,7 @@ int calculateSum(int X, int N) {
 int main() {
     int X, N;
     scanf("%d %d", &X, &N);
-
+    
     int result = calculateSum(X, N);
     printf("%d\n", result);
 
