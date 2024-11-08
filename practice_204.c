ed1623dc469c9820a37ff62bec237d5d1df2977c
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-08 10:57:28 +0600

    practice_204.c

--- practice_204.c
+++ practice_204.c
@@ -1,26 +1,4 @@
-//find the missing number
 #include <stdio.h>
-
-unsigned long long find_missing_number(unsigned long long M, unsigned long long A, unsigned long long B, unsigned long long C) {
-    unsigned long long product_ABC = A * B * C;
-
-    if (product_ABC == 0) {
-        if (M == 0) {
-            return 0;
-        } 
-        else {
-            return -1;
-        }
-    } 
-    else {
-        if (M % product_ABC == 0) {
-            return M / product_ABC;
-        } 
-        else {
-            return -1;
-        }
-    }
-}
 
 int main() {
     int T;
@@ -29,8 +7,27 @@ int main() {
     while (T--) {
         unsigned long long M, A, B, C;
         scanf("%llu %llu %llu %llu", &M, &A, &B, &C);
-        unsigned long long result = find_missing_number(M, A, B, C);
-        printf("%llu\n", result);
+        
+        unsigned long long product_ABC = A * B * C;
+        
+        // Check if product_ABC is zero
+        if (product_ABC == 0) {
+            // If M is also zero, the missing number D can be considered as 0
+            if (M == 0) {
+                printf("0\n");
+            } else {
+                // Otherwise, no valid missing number can satisfy the equation
+                printf("-1\n");
+            }
+        } else {
+            // Check if M is divisible by product_ABC
+            if (M % product_ABC == 0) {
+                unsigned long long D = M / product_ABC;
+                printf("%llu\n", D);
+            } else {
+                printf("-1\n");
+            }
+        }
     }
     
     return 0;
