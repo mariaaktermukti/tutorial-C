c6ae2a97409a4526e17f18797a47feb4257a5376
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-11-15 22:55:29 +0600

    contest_5.c

--- contest_5.c
+++ contest_5.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 int main() {
     int n, m;
     scanf("%d %d", &n, &m);
 
     int grid[n][m];
+
+  
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             scanf("%d", &grid[i][j]);
@@ -11,23 +14,31 @@ int main() {
     }
 
     int peak_count = 0;
-    int peaks[n * m][2]; 
+    int peaks[n * m][2];
+
+ 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            int is_peak = 1;
+            int is_peak = 1; 
 
+        
             if (i > 0 && grid[i][j] <= grid[i-1][j]) {
                 is_peak = 0;
             }
+        
             if (i < n-1 && grid[i][j] <= grid[i+1][j]) {
                 is_peak = 0;
             }
+      
             if (j > 0 && grid[i][j] <= grid[i][j-1]) {
                 is_peak = 0;
             }
+        
             if (j < m-1 && grid[i][j] <= grid[i][j+1]) {
                 is_peak = 0;
             }
+
+        
             if (is_peak) {
                 peaks[peak_count][0] = i + 1;
                 peaks[peak_count][1] = j + 1;
@@ -35,9 +46,11 @@ int main() {
             }
         }
     }
+
     printf("%d\n", peak_count);
     for (int i = 0; i < peak_count; i++) {
         printf("%d %d\n", peaks[i][0], peaks[i][1]);
     }
+
     return 0;
 }
